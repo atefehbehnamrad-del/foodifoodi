ced6634667fd1da482d406589df9a8d2928859cf
Atefeh <atefehbehmanrad@gmail.com>
2026-07-04 20:03:26 +0330

    change

--- Customer.cpp
+++ Customer.cpp
@@ -1,7 +1,12 @@
 #include "Customer.h"
 #include "NormalLevel.h"
-
+#include "SilverLevel.h"
+#include "GoldLevel.h"
+#include "VipLevel.h"
 #include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,6 +15,9 @@ Customer::Customer(int id, const string& username,const string& password,
     : User(id, username, password, name, UserRole::CUSTOMER){
     level = new NormalLevel();
     points = 0;
+    order = 0;
+    registerDate = time(nullptr);
+    previousOrder = time(nullptr);
 }
 
 Customer::~Customer(){
@@ -20,15 +28,29 @@ int Customer::getPoints() const{
     return points;
 }
 
+void Customer::setPoints(int points){
+    this->points = points;
+}
+
 void Customer::addPoints(int point){
-    points += point;
+    if(points <= 0){
+        retrun;
+    }
+    this->points += point;
+    cout << "points add final" << this->points << endl;
+    checkAndUpgrade();
 }
 
 void Customer::removePoints(int point){
-    points -= point;
+    if(points <= 0){
+        return;
+    }
+    this->points -= point;
 
-    if (points < 0)
-        points = 0;
+    if (this->points < 0)
+        this->points = 0;
+        cout << "points removed final " << this->points << endl;
+    checkAndDowngrade();
 }
 
 MembershipLevel* Customer::getLevel() const{
@@ -36,8 +58,58 @@ MembershipLevel* Customer::getLevel() const{
 }
 
 void Customer::setLevel(MembershipLevel* newLevel){
+    if(newLevel == nullptr){
+        return;
+    }
+    string oldLevelName = level->getLevelName;
+    string newLevelNAme = newlevel->getLevelName;
+
+    addLevelHistory(oldLevelName , newLevelName , "level change");
     delete level;
     level = newLevel;
+    cout << "your new level is " << newLevelName << endl;
+}
+
+int Customer::getPointsNextLevel() const {
+    if(point < 100){return 100 - points;}
+    else if(points < 300){return 300 - points;}
+    else if(points < 700){return 700 - points;}
+    else return 0;
+}
+
+void Customer::checkAndUpgrade() {
+    MembershipLevel* newLevel = nullptr;
+    string newLevelName;
+    
+    if (points >= 700 && dynamic_cast<VipLevel*>level == nullptr) {
+        newLevel = new VipLevel();
+        newLevelName = "VIP";
+    } else if (points >= 300 && dynamic_cast<GoldLevel*>level == nullptr) {
+        newLevel = new GoldLevel();
+        newLevelName = "Gold";
+    } else if (points >= 100 && dynamic_cast<SilverLevel*>level == nullptr) {
+        newLevel = new SilverLevel();
+        newLevelName = "Silver";
+    }
+    
+    if (newLevel != nullptr) {
+        string oldLevelName = level->getLevelName();
+        
+        addLevelHistory(oldLevelName, newLevelName, "upgrade");
+        
+        delete level;
+        level = newLevel;
+        
+        cout << "CONGRATULATIONS! UPGRADE!" << endl;
+        cout << "your old level was" << oldLevelName << endl;
+        cout << "your new level is" << newLevelNAme << endl;
+        cout << "benefits:" << endl;
+        cout << "Discount: " << level->getDiscount() << "%" << endl;
+        cout << "Ersal: " << level->getErsalCost() << endl;
+        cout << "Point Zarib" << level->getPointZarib() << endl;
+        cout << "Coupons: " << level->getMonthlyCoupons() << endl;
+        MonthlyCoupons();
+    }
 }
 
 void Customer::showMenu(){
--- Customer.h
+++ Customer.h
@@ -4,13 +4,39 @@
 #include "User.h"
 #include "MembershipLevel.h"
 #include "NormalLevel.h"
+#include "SilverLevel.h"
+#include "GoldLevel.h"
+#include "VipLevel.h"
 #include <string>
+#include <vector>
+#include <ctime>
+
+using namespace std;
+
+struct Coupons{
+    string code;
+    int discount;
+    time_t date;
+    string description;
+    bool isUsed;
+};
+
+struct ChangeLevel{
+    string pastLevel;
+    string newLevel;
+    time_t dateChange;
+    string reason;
+}
 
 class Customer : public User {
 private:
     MembershipLevel* level;
     int points;
-
+    vector<ChangeLevel> levelHistory;
+    vector<Coupon> coupons;
+    time_t previousOrder; 
+    time_t registerDate;
+    int orders;
 public:
     Customer(int id, const std::string& username, const std::string& password,
              const std::string& name);
@@ -18,12 +44,46 @@ public:
     ~Customer();
 
     int getPoints() const;
-    void addPoints(int point);
-    void removePoints(int point);
-
+    void setPoints(int points);
+    void addPoints(int points);
+    void removePoints(int points);
+    
     MembershipLevel* getLevel() const;
     void setLevel(MembershipLevel* newLevel);
     void showMenu() override;
+    
+    string getCurrentLevel() const;
+    void checkAndUpgrade();
+    void checkAndDowngrade();
+    int getPointsNextLevel() const;
+    
+    double FinalPrice(double basePrice) const;
+    int calculateErsalCost() const;
+    int calculateEarnedPoints(double orderAmount) const;
+    double calculateDiscount(double basePrice) const;
+    
+    void MonthlyCoupons();
+    vector<Coupons> getAvailableCoupons() const;
+    bool useCoupon(const string& code);
+    int getAvailableCouponCount() const;
+    void addCoupon(const Coupons& coupon);
+    
+    void addLevelHistory(const string& oldLevel, const string& newLevel, const string& reason);
+    vector<ChangeLevel> getLevelHistory() const;
+    void showLevelHistory() const;
+    
+    string getStage() const;
+    bool isFrequentBuyer() const;
+    bool isNightCustomer() const;
+    bool isLoyalMember() const;
+    
+    void showProfile() const;
+    void updateLastOrderDate();
+    void incrementConsecutiveOrders();
+    time_t getLastOrderDate() const;
+    void setLastOrderDate(time_t date);
+    int getConsecutiveOrders() const;
+    void setConsecutiveOrders(int count);
 };
 
 #endif

2ac5d9e4dc0470d0b75c54eb45b218487772f6f5
Atefeh <atefehbehmanrad@gmail.com>
2026-07-04 17:00:50 +0330

    new file

---
+++ VipLevel.cpp
@@ -0,0 +1,23 @@
+#include "VipLevel.h"
+
+using namespace std;
+
+string VipLevel::getLevelName() const{
+    return "VIP";
+}
+
+double VipLevel::getPointZarib() const{
+    return 2.0;
+}
+
+double VipLevel::getDiscount() const{
+    return 0.15;
+}
+
+int VipLevel::getErsalCost() const{
+    return 0;
+}
+
+int VipLevel::getNextLevel() const{
+    return -1;
+}
---
+++ VipLevel.h
@@ -0,0 +1,17 @@
+#ifndef VIPLEVEL_H
+#define VIPLEVEL_H
+
+#include "MembershipLevel.h"
+
+class VipLevel : public MembershipLevel
+{
+public:
+
+    string getLevelName() const override;
+    double getPointZarib() const override;
+    double getDiscount() const override;
+    int getErsalCost() const override;
+    int getNextLevel() const override;
+};
+
+#endif

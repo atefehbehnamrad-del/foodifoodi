fe16fccaa56d07c9159f3c76862a90c6e594e0de
Atefeh <atefehbehmanrad@gmail.com>
2026-07-05 17:44:28 +0330

    new

---
+++ Order.h
@@ -0,0 +1,69 @@
+#ifndef ORDER_H
+#define ORDER_H
+
+#include <vector>
+#include <string>
+#include <ctime>
+
+#include "OrderItem.h"
+
+enum class OrderStatus {
+    AWAITING,
+    PREPARING,
+    READY_TO_SEND,
+    DELIVERED
+};
+
+class Order {
+private:
+    int id;
+    int userId;
+    int restaurantId;
+
+    OrderStatus status;
+
+    std::vector<OrderItem> items;
+
+    double total;
+    time_t orderTime;
+    double finalPrice;
+    int earnPoints;
+    bool cancled;
+
+
+public:
+    Order(int id,int userId,int restaurantId);
+
+    Order(int id,int userId,int restaurantId,OrderStatus status,
+          const std::vector<OrderItem>& items,double total,
+          time_t orderTime);
+
+    int getId() const;
+    int getUserId() const;
+    int getRestaurantId() const;
+
+    OrderStatus getStatus() const;
+
+    double getTotal() const;
+    time_t getOrderTime() const;
+
+    const std::vector<OrderItem>& getItems() const;
+
+    void setStatus(OrderStatus status);
+    void addItem(MenuItem* item,int count);
+    void removeItem(int itemId);
+    double calculateTotal() const;
+    void display() const;
+
+    double getFainalPrice() const {
+        return finalPrice;
+    }
+    int getEarnPoints() const {
+        return earnPoints;
+    }
+    bool isCancled(bool cancle){
+        cancled = cancle;
+    }
+};
+
+#endif

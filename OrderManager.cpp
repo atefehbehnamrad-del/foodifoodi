017df498d04737c1bdc3f72d708f083e369aa2de
Atefeh <atefehbehmanrad@gmail.com>
2026-07-06 17:25:56 +0330

    updateed the ordermanager.cpp

--- OrderManager.cpp
+++ OrderManager.cpp
@@ -45,7 +45,7 @@ OrderManager::OrderManager() : nextIdOrder(1){}
             }
             for(auto& order : orders){
                 if(order.getId() == orderId && order.getUserId() == customer->getId()){
-                    if (order.isCancelled()) {
+                    if (order.isCancled()) {
                     cout << "order cancelled!" << endl;
                     return false;
                 }
@@ -64,18 +64,106 @@ OrderManager::OrderManager() : nextIdOrder(1){}
             if(payingPoints > 0){
                 coustomer->removePoints(payingPoints);
             }
-            
+            order.setCancle(true);
+            order.setStatus(orderStatus::CANCELLED);
+            cout << "new point" << payingPoints << endl;
+            cout << "your order cancled" << endl;
+            return true;
         }
     } 
+    cout << "the order didnt find" << endl;
+    return false;
 }
-        double OrderManager::calFinalPrice(Customer* customer ,const Order& order);
-        int OrderManager::calEarnPoints(Customer* customer , const Order& order);
-        std::vector<Order> getCustomerOrder(int customerId) const;
-        void OrderManager::showTheOrder(int customerId) const;
-        Order* getOrderById(int orderId);
-        int OrderManager::getactiveOrder() const;
-        int OrderManager::getTotalCount() const{
-            return orders.size();
+        double OrderManager::calFinalPrice(Customer* customer ,const Order& order){
+            if(customer == nullptr){
+                return order.getTotal();
+            }
+            double firstPrise = order.getTotal();
+            double discount = customer->calculatediscount(firstPrice);
+            int ersal = customer->calculateErsalCost();
+            return firstPrice - discount + ersal;
         }
-        double OrderManager::getTotalAllPrice() const;
-        void OrderManager::updateStatus(int orderId , OrderStatus , newStatus);
+        int OrderManager::calEarnPoints(Customer* customer , const Order& order){
+            if(customer == nullptr){
+                return customer->calEarnPoints(order.getTotal());
+            }
+        }
+        vector<Order> OrderManager::getCustomerOrder(int customerId) const{
+            vector<Order> couse;
+            auto it = coustomerOrder.find(coustomerId);
+            if(it != customerOrder.end()){
+                for(int orderId : it->second){
+                    for(const auto& order : orders){
+                        if(order.getId() == orderId){
+                            couse.push_back(order);
+                            break;
+                        }
+                    }
+                }
+            }
+            return couse;
+        }
+        void OrderManager::showTheOrder(int customerId) const{
+            auto customerOrderList = getCustomerOrder(customerId);
+            if(customerOrderList.empty()){
+                cout << "there is no order" << endl;
+                return;
+            }
+            cout << "----- History order -----" << endl;
+            for(const auto& order : customerOrderList){
+                order.display();
+            }
+        }
+        Order* OrderManager::getOrderById(int orderId){
+            for(auto& order : orders){
+                if(order.getId() == orderId){
+                    return &order;
+                }
+            }
+            return nullptr;
+        }
+        int OrderManager::getactiveOrder() const{
+            int number = 0;
+            for(const auto& order : orders){
+                if(order.getStatus != OrderStatus::DELIVERD && !order.isCancled()){
+                    number++;
+                }
+            }
+            return number;
+        }
+        double OrderManager::getTotalAllPrice() const{
+            double total = 0;
+            for(const auto& order : orders){
+                if(!order.isCancled){
+                    total += order.getFinalPrice();
+                }
+            }
+            return total;
+        }
+        void OrderManager::updateStatus(int orderId , OrderStatus , newStatus){
+            Order* order = getOrderById(orderId);
+            if(order == nullptr){
+                cout << "there is no order" << endl;
+                return;
+            }
+            if(order->isCancled()){
+                cout << "the order cancled" << endl;
+                return;
+            }
+            order->setstatus(newStatus);
+            cout << "order id" << orderId << "status updated to :";
+            if (newStatus == OrderStatus::AWAITING) {
+                cout << "Awaiting";
+            } else if (newStatus == OrderStatus::PREPARING) {
+                    cout << "Preparing";
+            } else if (newStatus == OrderStatus::READY_TO_SEND) {
+                    cout << "Ready to Send";
+            } else if (newStatus == OrderStatus::DELIVERED) {
+                    cout << "Delivered";
+            } else if (newStatus == OrderStatus::CANCELLED) {
+                    cout << "Cancelled";
+            } else {
+                    cout << "Unknown";
+            }
+            cout << endl;
+        }
